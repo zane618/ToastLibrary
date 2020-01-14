91274b59ee8617b7e58179453526f369013815bc
zhangshi <123zhangshi>
2020-01-14 17:19:59 +0800

    试试Application里能不能依赖到 证明：无法依赖到

--- app/src/main/java/com/example/toastlibrary/ApplicationToast.java
+++ app/src/main/java/com/example/toastlibrary/ApplicationToast.java
@@ -7,6 +7,11 @@ import android.widget.Toast;
  * create by zhangshi on 2020/1/14.
  */
 public class ApplicationToast {
+    /**
+     * 这里是无法被依赖到的
+     * @param context
+     * @param content
+     */
     public static void show(Context context, String content) {
         Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
     }
